45ee41cd2ff8b4c4e8289aaa9399ff9a591f1183
Qizzzzzzz <904859162@qq.com>
2024-01-09 22:56:36 +0800

    docs: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    hmr: true,
+  },
+  base: "./",
   plugins: [react()],
+  build: {
+    sourcemap: true,
+    outDir: "docs",
+  },
 })
